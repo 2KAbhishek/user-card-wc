bfb074e3382591286d576eb4da2db5f9b0186a97
Abhishek Keshri <keshri.abhishek63@gmail.com>
2021-05-23 16:00:36 +0530

    Use custom styling

--- userCard.js
+++ userCard.js
@@ -1,7 +1,21 @@
+const template = document.createElement("template");
+template.innerHTML = `
+    <style>
+    h3 {
+      color: silver;
+    }
+    </style>
+    <div class="user-card">
+        <h3> </h3>
+    </div>
+`;
+
 class UserCard extends HTMLElement {
   constructor() {
     super();
-    this.innerHTML = `<h2>${this.getAttribute("name")}</h2>`;
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
+    this.shadowRoot.querySelector("h3").innerText = this.getAttribute("name");
   }
 }
 
