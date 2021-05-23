a346ebec8b42d5c79d2c4d40158d2445a0fdc7a2
Abhishek Keshri <keshri.abhishek63@gmail.com>
2021-05-23 16:33:01 +0530

    Add styling

--- userCard.js
+++ userCard.js
@@ -1,19 +1,45 @@
 const template = document.createElement("template");
 template.innerHTML = `
     <style>
-    h3 {
-      color: silver;
+    .user-card {
+       background: #eee;
+      color: black;
+      width: 500px;
+      display: grid;
+      grid-template-columns: 1fr 2fr;
+      grid-gap: 10px;
+      padding: 20px;
+      margin: 20px;
+      border: 2px solid dodgerblue;
+      border-radius: 20px;
     }
 
-    img {
+    .user-card img {
         border-radius: 50%;
         width: 200px;
         height: auto;
+        border: 2px solid dodgerblue;
+    }
+
+    .user-card button {
+        cursor: pointer;
+        border: 0;
+        background-color: dodgerblue;
+        color: white;
+        border-radius: 10px;
     }
     </style>
+
     <div class="user-card">
         <img>
+        <div>
         <h3> </h3>
+        <div class="info">
+            <p>JOB</p>
+            <p>EMAIL</p>
+        </div>
+        <button id="toggle-info">Hide Info</button>
+        </div>
     </div>
 `;
 
